0134ac43e256969a6346eb2e4f6897c6621b5509
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-09-04 11:46:40 +0200

    Add #print_board

--- chess.rb
+++ chess.rb
@@ -6,6 +6,28 @@ class Game
     @board = Array.new(8) { Array.new(8, nil) }
   end
 
+  def print_board
+    @board.each do |subarr|
+      counter = 0
+      subarr.each do |value|
+        counter += 1
+        if value.nil?
+          if counter == 8
+            print "[   ]\n"
+          else
+            print "[   ] "
+          end
+        else
+          if counter == 7
+            print "[ #{value.color} ]\n"
+          else
+            print "[ #{value.color} ] "
+          end
+        end
+      end
+    end
+  end
+
   def test_piece
     p Rook.new('white', 2, 2)
   end
@@ -14,3 +36,4 @@ end
 
 game = Game.new
 game.test_piece
+game.print_board
