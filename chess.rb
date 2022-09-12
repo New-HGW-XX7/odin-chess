8138ee8ac4837167e1435012184e843929358046
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-09-12 16:16:04 +0200

    Set up checkmate check

--- chess.rb
+++ chess.rb
@@ -140,25 +140,36 @@ class Game
   end
 
   def is_checkmate?(color_of_king)
-    self.generate_legal_moves_all
-    possible_threats = []
+    #self.generate_legal_moves_all
+    pieces_of_defender = []
     king_row = nil
     king_column = nil
     king = nil
     self.board.each do |row|
       row.each do |field|
-        king_row = field.row if !field.nil? and field.type == 'king' and field.color == color_of_king
-        king_column = field.column if !field.nil? and field.type == 'king' and field.color == color_of_king
-        possible_threats << field unless field.nil? or field.color == color_of_king
+        pieces_of_defender << field if !field.nil? and field.color == color_of_king and field.type != 'king' # Gets all pieces except king
+        pieces_of_defender.unshift(field) if !field.nil? and field.color == color_of_king and field.type == 'king' # Sets king on position array.first
+        king_row = pieces_of_defender.first.row
+        king_column = pieces_of_defender.first.column
       end
     end
-    king = self.board[king_row, king_column]
-    return false if king.has_legal_moves?
+
+    evaluation_array = []
+    pieces_of_defender.each do |piece|
+      piece.legal_moves.each do |set, subset|
+        subset.each do |move|
+          temp_game = Game.new
+          copy_of_mainboard = Marshal.load( Marshal.dump(@board) )
+          temp_game.board = copy_of_mainboard
+          temp_game.move(piece.row, piece.column, move.first, move.last)
+
+          evaluation_array << temp_game.is_king_threatened?(color_of_king)
+        end
+      end
+    end
+    evaluation_array.include?(false) ? false : true
   end
 
-    # Extract pathss of attack from possible_threats
-    # Further up create an array of own pieces except king
-    # For each path check if any of these pieces can intercept path or kill threat
     # Simulate each interception and check if king is still threatened -> if still threatened, checkmate
 
   def play
@@ -177,9 +188,14 @@ class Game
     # Evaluate check and checkmate
     if self.is_king_threatened?(enemy_color)
       puts 'Check'
+
+      if self.is_checkmate?(enemy_color)
+        puts 'CHECKMATE'
+        # checkmate = true
+      end
     end
-    #  checkmate = self.is_checkmate?(enemy_color)
-    #end
+
+    # if self.is_king_threatened(enemy_color) returns false check for tie
 
     self.print_board
     case player_color
@@ -192,35 +208,6 @@ class Game
     end
     end
   end
-  # def play
-  #   Set up board with piece objects
-  #   Generate legal moves for every piece
-  #   Set player_color to white
-  #   - Loop
-  #   Prompt player with color to select piece #select_piece(player_color)
-  #     -> if field nil or piece not own color or has no legal moves* repeat prompt - *if piece is king check if only left -> tie
-
-  #   Assign variable holding piece || selected_piece = return of previous prompt
-
-  #   Prompt player to select target field -> #select_targetfield(selected_piece)
-  #     1. Check if target is in legal moves of piece -> #is_in_legal?(target coords) || selected_piece.is_in_legal?(x, y)
-
-  #     2. Check if move will produce check for self -> either #is_white_king_threatened?(copy of board with move done) or #is_black_king_threatened?(copy)
-  #     3. If criteria met perform move -> #move_piece(piece coords, target coords)
-
-
-  #   Check if enemy is checked -> #is_white/black_king_threatened?(board)
-
-  #     If yes, declare check and evaluate checkmate
-  #     #is_checkmate?(board, enemyking)
-  #       if king has legal moves -> false
-  #       else determine path of attack + attacking pieces coords
-  #         -> can any own piece intercept?
-  #         if yes -> false
-  #         else true
-  #   If checkmate true break loop
-  #   If checkmate false change player_color
-  # end
 
 
         
@@ -229,18 +216,3 @@ end
 
 game = Game.new
 game.play
-# # game.board[1][1] = Rook.new('white', 1, 1)
-# # game.board[3][1] = Rook.new('white', 3, 1)
-# game.board[3][2] = Rook.new('white', 3, 2)
-# game.board[3][1] = Rook.new('white', 3, 1)
-# game.board[3][3] = Rook.new('white', 3, 3)
-# # game.board[2][0] = Rook.new('black', 2, 0)
-# # game.board[5][2] = Rook.new('white', 5, 2)
-# # game.board[2][5] = Rook.new('white', 2, 5)
-
-
-
-# king = game.test_piece
-# game.board[2][2] = king
-# game.print_board
-# p king
